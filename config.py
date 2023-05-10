ad3a29c941d41eb1fe9030b1e6a921009a26f303
Pp005 <119912597+PratikLPP@users.noreply.github.com>
2023-05-10 10:46:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,20 +4,10 @@ API_ID = API_ID = 26435700
 
 API_HASH = os.environ.get("API_HASH", "527cf5174e120a9093611bc69d7b7709")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6155879022:AAFkLrPCRcDMFyk5GetOPYLEn6tybX6gu28")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6216620700:AAHOg_-ovNsBgbMd7a58wbWO4SqJwVkOCoc")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", 5908818236))
 
-LOG = -870450132
-
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5908818236").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-ADMINS.append(OWNER)
-
-
+LOG = -1001651990917
