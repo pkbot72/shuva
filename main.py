226f45266cd460556454dd9290483bad65222e15
pkbot72 <101813111+pkbot72@users.noreply.github.com>
2023-07-15 20:09:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6216620700:AAHOg_-ovNsBgbMd7a58wbWO4SqJwVkOCoc",
-             api_id= 26435700,
-             api_hash= "527cf5174e120a9093611bc69d7b7709")
+             bot_token= "6364078339:AAFxDhT2WEiApC2R1P6ashF8Bcf23B6R04w",
+             api_id= 9741228,
+             api_hash= "6f339e8fa827a5147fa2ff03898d610c")
 
 
 @bot.on_message(filters.command(["start"])& ~filters.edited)
