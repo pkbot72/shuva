d68e0ef1b6167c762dd3b3c2f327b82298e1db9f
pkbot72 <101813111+pkbot72@users.noreply.github.com>
2023-07-15 20:05:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
-API_ID = API_ID = 26435700
+API_ID = API_ID = 9741228
 
-API_HASH = os.environ.get("API_HASH", "527cf5174e120a9093611bc69d7b7709")
+API_HASH = os.environ.get("API_HASH", "6f339e8fa827a5147fa2ff03898d610c)
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6216620700:AAHOg_-ovNsBgbMd7a58wbWO4SqJwVkOCoc")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6364078339:AAFxDhT2WEiApC2R1P6ashF8Bcf23B6R04w")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 5908818236))
+OWNER = int(os.environ.get("OWNER", 809150135))
 
-LOG = -1001651990917
+LOG = --1001879104776
